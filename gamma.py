dcc86b66b8024a1513c4890e1cb9d9e169108fdf
elroicarmel <elroica277@gmail.com>
2024-05-06 23:41:25 +0300

    Successful GREY

--- gamma.py
+++ gamma.py
@@ -9,6 +9,8 @@
         ........::..:::::..:::......::
 """
 from ex1_utils import LOAD_GRAY_SCALE
+import numpy as np
+import cv2
 
 
 def gammaDisplay(img_path: str, rep: int):
@@ -18,7 +20,23 @@ def gammaDisplay(img_path: str, rep: int):
     :param rep: grayscale(1) or RGB(2)
     :return: None
     """
-    pass
+
+    def gammaChange(val: int):
+        gamma = val / 100
+        print(gamma, val)
+        img_cpy = np.round(np.power(img_norm, gamma)*255).astype(np.uint8)
+        cv2.imshow('Gamma Correction', img_cpy)
+
+    img = cv2.imread(img_path)
+    if rep == LOAD_GRAY_SCALE:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    img_norm = img / 255
+
+    cv2.namedWindow('Gamma Correction', cv2.WINDOW_NORMAL)
+    cv2.createTrackbar('Gamma value', 'Gamma Correction', 0, 200, gammaChange)
+
+    cv2.waitKey(0)
 
 
 def main():
